fe4a00cfde9aba3c631e6311d05c00cf791766a4
Sehaj Naangal <snaangal@uwo.ca>
2023-11-25 12:25:33 -0500

    add local storage to save all user details

---
+++ my-app/src/App 2.jsx
@@ -0,0 +1,13 @@
+// components
+import UserProfile from "./components/UserProfile/UserProfile";
+
+// dependencies
+import { Routes, Route } from "react-router-dom"
+
+const App = () => {
+  return (
+
+  )
+}
+
+export default App;
---
+++ my-app/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,14 @@
+import "./LandingPage.css";
+
+import NavigationBar from "../Common/NavigationBar/NavigationBar";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <NavigationBar />
+        <h1>Landing page</h1>
+    </div>
+  )
+}
+
+export default LandingPage;
