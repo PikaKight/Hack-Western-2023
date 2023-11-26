a50b50fa84665ec926ff4d73dbb4ad75116e6790
Adrian <akoziski@uwo.ca>
2023-11-26 03:21:29 -0500

    adios background

--- my-app/src/components/Common/NavigationBar/NavigationBar.jsx
+++ my-app/src/components/Common/NavigationBar/NavigationBar.jsx
@@ -22,7 +22,7 @@ const NavigationBar = () => {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>App Name</Typography>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}></Typography>
           <Button color="inherit" onClick={goToHomePage}>Home</Button>
           <Button color="inherit" onClick={goToAccountPage}>Login</Button>
           <Button color="inherit" onClick={goToMatchingPage}>Matching</Button>
