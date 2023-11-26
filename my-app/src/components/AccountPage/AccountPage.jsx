6e42c287ba2e3248acf0f249fca8610f1031c6a7
Sehaj Naangal <snaangal@uwo.ca>
2023-11-25 20:57:08 -0500

    create user tags and can save these in local storage

--- my-app/src/components/AccountPage/AccountPage.jsx
+++ my-app/src/components/AccountPage/AccountPage.jsx
@@ -4,6 +4,8 @@ import NavigationBar from "../Common/NavigationBar/NavigationBar";
 import UserAuthentication from "../UserAuthentication/UserAuthentication";
 import { useEffect, useState } from "react";
 import Checkbox from '@mui/material/Checkbox';
+import { useNavigate } from "react-router-dom";
+import CodeSnippetTags from "../CodeSnippetTags/CodeSnippetTags";
 
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
@@ -13,6 +15,9 @@ const AccountPage = () => {
   const [username, setUsername] = useState('');
   const [codeSnippet, setCodeSnippet] = useState('');
   const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
+  const [codeOverview, setCodeOverview] = useState('');
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const savedIsUserLoggedIn = localStorage.getItem("IS_USER_LOGGED_IN");
@@ -29,6 +34,12 @@ const AccountPage = () => {
       const parsedCodeSnippet = JSON.parse(savedCodeSnippet);
       setCodeSnippet(parsedCodeSnippet);
     }
+
+    const savedCodeOverview = localStorage.getItem("CODE_OVERVIEW");
+    if (savedCodeOverview) {
+      const parsedCodeOverview = JSON.parse(savedCodeOverview);
+      setCodeOverview(parsedCodeOverview);
+    }
   }, [])
 
   const logOut = () => {
@@ -36,12 +47,23 @@ const AccountPage = () => {
     window.location.reload();
   }
 
-  const handleCodeSnippetChange = () => {
-    localStorage.setItem("CODE_SNIPPET", JSON.stringify(codeSnippet));
-  }
-
   const handleCheckboxChange = () => {
     setIsCheckboxChecked(!isCheckboxChecked);
+  }
+
+  const goToMatchingPage = () => {
+    if (isCheckboxChecked) {
+      navigate("/");
+    }
+    else {
+      alert("Please select checkbox");
+    }
+  }
+
+  const handleCodeOverviewChange = (event) => {
+    const codeOverview = event.target.value;
+    setCodeOverview(codeOverview);
+    localStorage.setItem("CODE_OVERVIEW", JSON.stringify(codeOverview));
   }
 
   const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
@@ -59,13 +81,18 @@ const AccountPage = () => {
             value={codeSnippet}
             onChange={(content) => setCodeSnippet(content)}
           />
+          <div>
+            <h4>Brief overview of what this code does.</h4>
+            <textarea cols="80" rows="5" value={codeOverview} onChange={handleCodeOverviewChange}></textarea>
+          </div>
+          <h4>Add tags</h4>
+          <CodeSnippetTags />
           <div className="account-page-checkbox">
             <Checkbox {...label} checked={isCheckboxChecked} onChange={handleCheckboxChange} />
             <p>I acknowledge that the provided code is a result of my own individual effort and creativity.</p>
           </div>
           <div className="account-page-end-buttons">
-            <button onClick={handleCodeSnippetChange}>Save</button>
-            <button>Start matching!</button>
+            <button onClick={goToMatchingPage}>Start matching!</button>
           </div>
           <button onClick={logOut}>Log out</button>
         </div>
