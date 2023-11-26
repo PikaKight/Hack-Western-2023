7b91e20c740300c7146c21d6e9197e4287183d86
Sehaj Naangal <snaangal@uwo.ca>
2023-11-26 05:39:01 -0500

    ad

--- my-app/src/components/Common/NavigationBar/NavigationBar.jsx
+++ my-app/src/components/Common/NavigationBar/NavigationBar.jsx
@@ -20,11 +20,11 @@ const NavigationBar = () => {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static">  
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}></Typography>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>App Name</Typography>
           <Button color="inherit" onClick={goToHomePage}>Home</Button>
-          <Button color="inherit" onClick={goToAccountPage}>Account</Button>
+          <Button color="inherit" onClick={goToAccountPage}>Login</Button>
           <Button color="inherit" onClick={goToMatchingPage}>Matching</Button>
         </Toolbar>
       </AppBar>
